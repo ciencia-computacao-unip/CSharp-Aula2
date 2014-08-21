c4201e0e85081102034acbd5a581aab7be9bb122
Victor Perin <victor_perin@nerdhitech.com.br>
2014-08-21 08:37:31 -0300

    Exercício 1

--- Program.cs
+++ Program.cs
@@ -7,6 +7,12 @@ using System.Threading.Tasks;
 namespace CSharpAula2{
     class Program{
         static void Main(string[] args){
+            //Exercício 1
+            Console.WriteLine("1) Faça um programa que informe ao usuário que deverá digitar um número e o programa deverá exibir o resultado do cálculo do quadrado deste número.");
+            int numero;
+            numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("O quadrado do número é " + numero * numero);
+            //fim do exercício 1
         }
     }
 }
